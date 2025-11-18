189b50e5569021655ae53ea1edafeb6071039af4
0hm☘️ <git8ankanroy@gmail.com>
2025-11-18 19:58:28 +0530

    WIP

--- tests/bugs/internally-connected-pads.test.ts
+++ tests/bugs/internally-connected-pads.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from "bun:test"
 import { AutoroutingPipelineSolver } from "lib"
 import type { SimpleRouteJson } from "lib/types"
 import { getLastStepSvg } from "../fixtures/getLastStepSvg"
-import srj from "../../examples/bug-reports/internally-connected-pads/internally-connected-pads.json"
+import srj from "../../examples/internally-connected-pads/internally-connected-pads.json"
 
 test("NetToPointPairsSolver with internally connected pads", () => {
   const solver = new AutoroutingPipelineSolver(srj as SimpleRouteJson)
